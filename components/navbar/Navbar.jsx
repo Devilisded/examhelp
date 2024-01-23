09944b9718b96becef829cc31e985456194bb0e4
Mayank74133 <155705076+Mayank74133@users.noreply.github.com>
2024-01-23 14:43:51 +0530

    Update Navbar.jsx

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "@/public/images/  .webp";
+import logo from "@/public/images/logo.webp";
 import { IconMenu2 } from "@tabler/icons-react";
 
 const Navbar = () => {
