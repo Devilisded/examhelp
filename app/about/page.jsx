97cba657b6529ec2ca6abafc6eb0175e677874bd
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-03-11 17:13:19 +0530

    update in page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -12,7 +12,9 @@ const Aboutus = () => {
   return (
     <div>
       <Navbar />
+
       <ServiceHero title="Testing Dynamic Title for the service hero" />
+
       <section class="bg-white dark:bg-gray-900">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
@@ -45,12 +47,119 @@ const Aboutus = () => {
           </div>
         </div>
       </section>
+
       <div className="py-10">
         <h1 className="text-4xl text-center font-semibold mb-10">
           Our Experts
         </h1>
         <ExpertCarousel slides={SLIDES} options={OPTIONS} />
       </div>
+
+
+
+      <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+ 
+        <section class=" container mb-32">
+          <h1 class="text-4xl font-bold">
+            An intriguing title for an interesting article
+          </h1>
+          <div className="mt-1">
+                    <span className="inline-block w-96 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+
+          <p class="mb-5 mt-3 text-slate-700">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            harum tempore cupiditate asperiores provident, itaque, quo ex iusto
+            rerum voluptatum delectus corporis quisquam maxime a ipsam nisi
+            sapiente qui optio! Dignissimos harum quod culpa officiis suscipit
+            soluta labore! Expedita quas, nesciunt similique autem, sunt,
+            doloribus pariatur maxime qui sint id enim. Placeat, maxime labore.
+            Dolores ex provident ipsa impedit, omnis magni earum. Sed fuga ex
+            ducimus consequatur corporis, architecto nesciunt vitae ipsum
+            consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus
+            similique eum vero in est velit quasi pariatur blanditiis incidunt
+            quam.
+          </p>
+
+
+          <h1 class="text-4xl font-bold ">
+            An intriguing title for an interesting article
+          </h1>
+          <div className="mt-1">
+                    <span className="inline-block w-96 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+
+          <p class="mb-5 mt-3 text-slate-700">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            harum tempore cupiditate asperiores provident, itaque, quo ex iusto
+            rerum voluptatum delectus corporis quisquam maxime a ipsam nisi
+            sapiente qui optio! Dignissimos harum quod culpa officiis suscipit
+            soluta labore! Expedita quas, nesciunt similique autem, sunt,
+            doloribus pariatur maxime qui sint id enim. Placeat, maxime labore.
+            Dolores ex provident ipsa impedit, omnis magni earum. Sed fuga ex
+            ducimus consequatur corporis, architecto nesciunt vitae ipsum
+            consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus
+            similique eum vero in est velit quasi pariatur blanditiis incidunt
+            quam.
+          </p>
+
+
+          <h1 class="text-4xl font-bold ">
+            An intriguing title for an interesting article
+          </h1>
+          <div className="mt-1">
+                    
+                    <span className="inline-block w-96 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+
+          <p class="mb-5 mt-3 text-slate-700">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            harum tempore cupiditate asperiores provident, itaque, quo ex iusto
+            rerum voluptatum delectus corporis quisquam maxime a ipsam nisi
+            sapiente qui optio! Dignissimos harum quod culpa officiis suscipit
+            soluta labore! Expedita quas, nesciunt similique autem, sunt,
+            doloribus pariatur maxime qui sint id enim. Placeat, maxime labore.
+            Dolores ex provident ipsa impedit, omnis magni earum. Sed fuga ex
+            ducimus consequatur corporis, architecto nesciunt vitae ipsum
+            consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus
+            similique eum vero in est velit quasi pariatur blanditiis incidunt
+            quam.
+          </p>
+
+          <h1 class="text-4xl font-bold ">
+            An intriguing title for an interesting article
+          </h1>
+          <div className="mt-1">
+                    
+                    <span className="inline-block w-96 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+
+          <p class="mb-5 mt-3 text-slate-700">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            harum tempore cupiditate asperiores provident, itaque, quo ex iusto
+            rerum voluptatum delectus corporis quisquam maxime a ipsam nisi
+            sapiente qui optio! Dignissimos harum quod culpa officiis suscipit
+            soluta labore! Expedita quas, nesciunt similique autem, sunt,
+            doloribus pariatur maxime qui sint id enim. Placeat, maxime labore.
+            Dolores ex provident ipsa impedit, omnis magni earum. Sed fuga ex
+            ducimus consequatur corporis, architecto nesciunt vitae ipsum
+            consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus
+            similique eum vero in est velit quasi pariatur blanditiis incidunt
+            quam.
+          </p>
+
+        </section>
+               </div>
+
+      
       <div className="w-full cbg ">
         <div className="w-full">
           <div className="z-50 text-center mx-auto max-w-[510px]">
@@ -63,38 +172,7 @@ const Aboutus = () => {
           <Carousel />
         </div>
       </div>
-      {/* <div className="flex justify-center h-[80vh] p-10">
-        <div className="w-[80%] flex gap-5">
-          <div className="flex flex-col gap-5">
-            <div className="h-full px-5 py-10 flex flex-col gap-10 bg-blue-100 rounded-xl">
-              <h1 className="text-7xl font-semibold">
-                New goals require new knowledge
-              </h1>
-              <div>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
-                nam dolorum maxime rem quasi quos dolorem, sequi, veniam ea
-                voluptates explicabo error iusto distinctio nisi, dignissimos
-                pariatur at sint nesciunt? Sequi, et.
-              </div>
-            </div>
-            <div className="h-1/3 flex gap-5">
-              <img
-                src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-                className="h-full w-full object-cover rounded-xl"
-              />
-              <div className="p-10 flex justify-center items-center bg-orange-400/90 rounded-xl text-white">
-                <IconArrowDownRight className="w-40 h-40" />
-              </div>
-            </div>
-          </div>
-          <img
-            src="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            className="w-1/3 object-cover rounded-xl"
-          />
-        </div>
-      </div> */}
+     
       <Footer />
     </div>
   );
