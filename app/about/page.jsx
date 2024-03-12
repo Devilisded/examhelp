7ac215ac6a6e44d3957e00097a2c0408cd026e0f
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-03-12 14:45:24 +0530

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,6 +3,7 @@ import Footer from "@/components/footer/Footer";
 import ServiceHero from "@/components/serviceHero/ServiceHero";
 import ExpertCarousel from "@/components/expertCarousel/ExpertCarousel";
 import Carousel from "@/components/carousel/Carousel";
+import Image from "next/image";
 import {
   IconChevronDown,
   IconClock2,
@@ -311,7 +312,54 @@ const Aboutus = () => {
           <Carousel />
         </div>
       </div>
-
+      <div className="bg-white py-10">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+            Trusted by the world’s most innovative teams
+          </h2>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+            <Image
+              className="col-span-2  w-auto object-contain lg:col-span-1 "
+              src="/images/uni1.webp"
+              alt="logo1"
+              width={158}
+              height={48}
+              loading="lazy"
+            />
+            <Image
+              className="col-span-2  w-auto object-contain lg:col-span-1"
+              src="/images/logo-un2.webp"
+              alt="logo2"
+              width="158"
+              height="48"
+              loading="lazy"
+            />
+            <Image
+              className="col-span-2  w-auto object-contain lg:col-span-1"
+              src="/images/logo-un3.webp"
+              alt="logo3"
+              width="158"
+              height="48"
+            />
+            <Image
+              className="col-span-2  w-auto object-contain sm:col-start-2 lg:col-span-1"
+              src="/images/logo-un4.webp"
+              alt="logo4"
+              width="158"
+              height="48"
+              loading="lazy"
+            />
+            <Image
+              className="col-span-2 col-start-2 w-auto object-contain sm:col-start-auto lg:col-span-1"
+              src="/images/logo-un5.webp"
+              alt="logo5"
+              width="158"
+              height="48"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
