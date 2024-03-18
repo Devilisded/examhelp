fcd2c6726abfa6cc5aca3d0cb5ccd2aa6663b694
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-03-18 16:50:46 +0530

    updates

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,7 +10,7 @@ const Page = () => {
   return (
     <div>
       <Header />
-      <div className="flex justify-center flex-col items-center text-5xl md:text-7xl  font-semibold py-8 mainDiv text-white">
+      {/* <div className="flex justify-center flex-col items-center text-5xl md:text-7xl  font-semibold py-8 mainDiv text-white">
         About Us
         <div className="flex items-center flex-col">
           <div className="text-xl  text-center font-thin py-2 w-[80%]">
@@ -22,9 +22,55 @@ const Page = () => {
             stress out of test prep by offering:
           </div>
         </div>
-      </div>
-
-      <div className="grid grid-cols-2 p-12 gap-8">
+      </div> */}
+      <section class="relative overflow-hidden bg-gradient-to-b from-blue-50 via-transparent to-transparent pb-12 pt-20 sm:pb-16 sm:pt-32 lg:pb-24 xl:pb-32 xl:pt-40">
+        <div class="relative z-10">
+          <div class="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
+            <svg
+              class="h-[60rem] w-[100rem] flex-none stroke-blue-600 opacity-20"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
+                  width="200"
+                  height="200"
+                  x="50%"
+                  y="50%"
+                  patternUnits="userSpaceOnUse"
+                  patternTransform="translate(-100 0)"
+                >
+                  <path d="M.5 200V.5H200" fill="none"></path>
+                </pattern>
+              </defs>
+              <svg x="50%" y="50%" class="overflow-visible fill-blue-50">
+                <path
+                  d="M-300 0h201v201h-201Z M300 200h201v201h-201Z"
+                  stroke-width="0"
+                ></path>
+              </svg>
+              <rect
+                width="100%"
+                height="100%"
+                stroke-width="0"
+                fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)"
+              ></rect>
+            </svg>
+          </div>
+        </div>
+        <div class="relative z-20 mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              About Us
+            </h1>
+            <h2 class="mt-6 text-lg leading-8 text-gray-600">
+              Choose a more eco-friendly way to hydrate with our 100% recyclable
+              cartons.
+            </h2>
+          </div>
+        </div>
+      </section>
+      <div className="grid grid-cols-2 px-12 pt-12 gap-8">
         <Image src={office} className="object-cover max-h-[400px]" />
         <div className="flex flex-col gap-6 items-start justify-start">
           <div className="text-md md:text-lg flex items-center gap-8">
@@ -56,8 +102,7 @@ const Page = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex flex-col-reverse lg:flex-row  md:mx-12 md:ml-24 gap-4 lg:gap-24 pb-10 md:pb-16">
+      <div className="grid grid-cols-2 px-12 gap-8">
         <div className="flex flex-col gap-4 justify-center mx-16 md:mx-0">
           <div className="text-lg md:text-2xl font-semibold ">
             At Exam Help Online, we want each student to achieve their full
--- app/referer/page.jsx
+++ app/referer/page.jsx
@@ -1,8 +1,14 @@
 import { headers } from "next/headers";
+import Link from "next/link";
 
 export default function Page() {
   const headersList = headers();
   const referer = headersList.get("referer");
 
-  return <div>Referer: {referer}</div>;
+  return (
+    <div>
+      Referer: {referer}
+      <Link href="/">Home</Link>
+    </div>
+  );
 }
