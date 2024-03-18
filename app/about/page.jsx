f1fd0742463347c286e3fc81495d520a3e6bdb5a
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-03-18 17:58:53 +0530

    about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -64,56 +64,89 @@ const Page = () => {
               About Us
             </h1>
             <h2 class="mt-6 text-lg leading-8 text-gray-600">
-              Choose a more eco-friendly way to hydrate with our 100% recyclable
-              cartons.
+              Welcome to Exam Help Online, your top place for exam prep and
+              tutoring services. We aim to help students succeed by providing
+              great exam prep tools and personalized academic support.
             </h2>
           </div>
         </div>
       </section>
-      <div className="grid grid-cols-2 px-12 pt-12 gap-8">
+      <div className="grid md:grid-cols-2 px-12 pt-12 gap-8">
         <Image src={office} className="object-cover max-h-[400px]" />
-        <div className="flex flex-col gap-6 items-start justify-start">
-          <div className="text-md md:text-lg flex items-center gap-8">
-            <IconAtom2 color="red" className="w-16 h-16" />
-            <div>
+        <div className="flex flex-col gap-4">
+          <p className="text-xl font-semibold">
+            Established in 2010, Exam Help Online was created by teachers who
+            understand students' challenges when prepping for big exams. We aim
+            to take the stress out of test prep by offering:
+          </p>
+          <ul className="text-lg list-disc">
+            <li className="py-4">
+              {" "}
               Complete study guides and cheat sheets covering key concepts for
               major standardized tests. Tutors and teachers design our study
               materials with years of test prep experience.
+            </li>
+            <li className="py-4">
+              {" "}
+              Access 24/7 to online practice tests modeled closely after the
+              real exams. Our tests are designed to help you identify strengths
+              and weaknesses so you can focus your study time effectively.
+            </li>
+            <li className="py-4">
+              {" "}
+              One-on-one tutoring with instructors who specialize in your exam.
+              Get personalized support, ask questions, review mistakes, and gain
+              confidence. Our tutors have scored in the top 10% on the exams
+              they teach.
+            </li>
+            <li className="py-4">
+              {" "}
+              On-demand homework help and subject tutoring. Whether you need
+              help mastering algebra, writing an essay, or studying for a
+              biology test, our tutors are here to help students of all ages and
+              grades.
+            </li>
+          </ul>
+          {/* <div className="flex flex-col gap-6 items-start justify-start">
+            <div className="text-md md:text-lg flex items-center gap-8">
+              <IconAtom2 color="red" className="w-16 h-16" />
+              <div>
+                Complete study guides and cheat sheets covering key concepts for
+                major standardized tests. Tutors and teachers design our study
+                materials with years of test prep experience.
+              </div>
             </div>
-          </div>
-          <div className="text-md md:text-lg flex items-center gap-8">
-            <IconAtom2 color="red" className="w-16 h-16" />
-            Access 24/7 to online practice tests modeled closely after the real
-            exams. Our tests are designed to help you identify strengths and
-            weaknesses so you can focus your study time effectively.
-          </div>
-          <div className="text-md md:text-lg flex items-center gap-8">
-            <IconAtom2 color="red" className="w-16 h-16" />
-            One-on-one tutoring with instructors who specialize in your exam.
-            Get personalized support, ask questions, review mistakes, and gain
-            confidence. Our tutors have scored in the top 10% on the exams they
-            teach.
-          </div>
-          <div className="text-md md:text-lg flex items-center gap-8">
-            <IconAtom2 color="red" className="w-16 h-16" />
-            On-demand homework help and subject tutoring. Whether you need help
-            mastering algebra, writing an essay, or studying for a biology test,
-            our tutors are here to help students of all ages and grades.
-          </div>
+            <div className="text-md md:text-lg flex items-center gap-8">
+              <IconAtom2 color="red" className="w-16 h-16" />
+              Access 24/7 to online practice tests modeled closely after the
+              real exams. Our tests are designed to help you identify strengths
+              and weaknesses so you can focus your study time effectively.
+            </div>
+            <div className="text-md md:text-lg flex items-center gap-8">
+              <IconAtom2 color="red" className="w-16 h-16" />
+              One-on-one tutoring with instructors who specialize in your exam.
+              Get personalized support, ask questions, review mistakes, and gain
+              confidence. Our tutors have scored in the top 10% on the exams
+              they teach.
+            </div>
+            <div className="text-md md:text-lg flex items-center gap-8">
+              <IconAtom2 color="red" className="w-16 h-16" />
+              On-demand homework help and subject tutoring. Whether you need
+              help mastering algebra, writing an essay, or studying for a
+              biology test, our tutors are here to help students of all ages and
+              grades.
+            </div>
+          </div> */}
         </div>
       </div>
-      <div className="grid grid-cols-2 px-12 gap-8">
-        <div className="flex flex-col gap-4 justify-center mx-16 md:mx-0">
-          <div className="text-lg md:text-2xl font-semibold ">
-            At Exam Help Online, we want each student to achieve their full
-            potential. We offer affordable test prep programs, flexible tutoring
-            options, and great customer service. Thousands of students have used
-            our services to get into their dream colleges and do well in school.
-          </div>
-          <div className="text-md lg:text-xl text-slate-700 font-semibold">
-            Contact us today to learn how we can customize a test prep or
-            tutoring program to match your specific needs and goals.
-          </div>
+      <div className="grid md:grid-cols-2 px-12 gap-8 place-items-center">
+        <div className="text-2xl">
+          At Exam Help Online, we want each student to achieve their full
+          potential. We offer affordable test prep programs, flexible tutoring
+          options, and great customer service. Thousands of students have used
+          our services to get into their dream colleges and do well in school.
+          Contact us today to learn how we can customize a test prep or tutoring
+          program to match your specific needs and goals.
           <div className="text-md lg:text-xl text-slate-600 ">
             We look forward to helping you succeed!
             <br />
