253251973d05ff447bf311f3b521f2a07ee59b4b
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-04-11 17:44:48 +0530

    updated disclaimer

--- app/disclaimer/page.jsx
+++ app/disclaimer/page.jsx
@@ -1,27 +1,67 @@
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+
 const page = () => {
   return (
-    <div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] pb-20">
-      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
-        <div class="mt-5 max-w-2xl text-center mx-auto">
-          <h1 class="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-            Book
-            <span class="bg-clip-text bg-gradient-to-tl from-blue-500 to-violet-500 text-transparent">
-              &nbsp;Gas
-            </span>
-          </h1>
-        </div>
-
-        <div></div>
-
-        <div class="mt-5 max-w-3xl text-center mx-auto">
-          <p class="text-lg text-gray-500 dark:text-gray-400">
-            kdjas ajsdkj ddkjs fa;ls jkds lkldsjfkdsjfk d kdsj fkasdj fkasd
-            fjkads jfkasd jaksd fjkasdj fs aks jkasdj fkasdj k jskdjs fkds
-            jfkdsjfkjiref errirjifji rfifjri frji rijfrijf fjdksfir rje firjir
-            jfijrifj irfj irfjirejf
-          </p>
+    <div className="w-full">
+      <Navbar />
+      <div className="mt-5">
+        <h1 className=" text-center mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-6xl text-blue-600">
+          Disclaimer
+        </h1>
+        <div>
+          <div className="px-10 md:px-0 text-center md:text-left text-slate-600 font-sans font-normal m-5 text-xl mx-auto max-w-7xl pb-10">
+            <p>
+              All the information on this website - https://examhelp.online - is
+              published in good faith and for general information purposes only.
+              Exam Help Online does not make any warranties about the
+              completeness, reliability, and accuracy of this information. Any
+              action you take based on the information you find on this website
+              (Exam Help Online) is strictly at your own risk. Exam Help Online
+              will not be liable for any losses or damages caused by using our
+              website.
+            </p>
+            <br></br>
+            <p>
+              From our website, you can visit other websites by following
+              hyperlinks to such external sites. While we strive to provide only
+              quality links to useful and ethical websites, we have no control
+              over the content and nature of these sites. These links to other
+              websites do not imply a recommendation for all the content found
+              on these sites. Site owners and content may change without notice
+              and occur before we can remove a link that may have gone 'bad'.
+            </p>
+            <br></br>
+            <p>
+              Please also be aware that when you leave our website, other sites
+              may have different privacy policies and terms beyond our control.
+              Please check the Privacy Policies of these sites and their "Terms
+              of Service" before engaging in any business or uploading any
+              information.
+            </p>
+            <br></br>
+            <h2 className="font-semibold">Consent</h2>
+            <p>
+              {" "}
+              Using our website, you consent to our disclaimer and agree to its
+              terms.
+            </p>
+            <br></br>
+            <h2 className="font-semibold">Update</h2>
+            <p>
+              Should we update, amend, or make any changes to this document,
+              those changes will be prominently posted here.
+            </p>
+            <br></br>
+            <p>
+              If you require more information or have any questions about our
+              site's disclaimer, please feel free to contact us by email at
+              info@examhelp.online.
+            </p>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
